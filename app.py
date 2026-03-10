73c1816272a8c790aa1c6170debedcc31ecf7a4c
juanda2210 <juandarias2204@gmail.com>
2026-03-10 16:57:22 -0500

    feat: :tada: Creacion archivo app
    Se creó el archivo app y se añade el menú base del proyecto

---
+++ app.py
@@ -0,0 +1,27 @@
+bool = True
+
+while bool == True:
+    print("--menu tareas--")
+    print("1. crear tarea")
+    print("2. listar tarea")
+    print("3. marcar tarea")
+    print("4. eliminar tarea")
+    print("5. Salir")
+
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        print("")
+    
+    elif opcion == "2":
+        print("")
+
+    elif opcion == "3":
+        print("")
+
+    elif opcion == "4":
+        print ("")
+    
+    elif opcion == "5":
+        print("Saliendo del programa...")
+        bool = False
